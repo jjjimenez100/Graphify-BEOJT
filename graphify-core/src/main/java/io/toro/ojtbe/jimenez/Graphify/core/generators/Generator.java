b88cb9b974cef1f6e1d2fcfd67407833570eaca7
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2019-02-18 14:02:03 +0800

    Added Generator interface :sparkles:

---
+++ graphify-core/src/main/java/io/toro/ojtbe/jimenez/Graphify/core/generators/Generator.java
@@ -0,0 +1,7 @@
+package io.toro.ojtbe.jimenez.Graphify.core.generators;
+
+import io.toro.ojtbe.jimenez.Graphify.core.GraphEntity;
+
+public interface Generator {
+    boolean generate(GraphEntity... graphEntities);
+}
