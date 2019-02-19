67f9636740e11b34070d3206adad6546f566a295
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2019-02-19 11:27:24 +0800

    Changed signature of generate for QueryGenerator :hammer:

--- graphify-core/src/main/java/io/toro/ojtbe/jimenez/Graphify/core/generators/QueryGenerator.java
+++ graphify-core/src/main/java/io/toro/ojtbe/jimenez/Graphify/core/generators/QueryGenerator.java
@@ -3,10 +3,12 @@ package io.toro.ojtbe.jimenez.Graphify.core.generators;
 import io.toro.ojtbe.jimenez.Graphify.core.GraphEntity;
 
 import java.util.List;
+import java.util.Map;
 
 interface QueryGenerator extends Generator {
     void generate(List<GraphEntity> graphEntities,
-                     String serviceClass,
-                     String servicePackage)
+                  Map<String, String> services,
+                  String packageName,
+                  String path)
             throws QueryGeneratorException;
 }
