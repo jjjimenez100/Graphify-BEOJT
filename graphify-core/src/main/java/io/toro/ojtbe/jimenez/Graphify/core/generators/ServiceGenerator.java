a44a6163433e85eb36371acb0829bbb347d411a4
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2019-02-20 14:52:03 +0800

    Removed unnecessary Generator interface :fire:

--- graphify-core/src/main/java/io/toro/ojtbe/jimenez/Graphify/core/generators/ServiceGenerator.java
+++ graphify-core/src/main/java/io/toro/ojtbe/jimenez/Graphify/core/generators/ServiceGenerator.java
@@ -1,8 +1,11 @@
 package io.toro.ojtbe.jimenez.Graphify.core.generators;
 
 import io.toro.ojtbe.jimenez.Graphify.core.GraphEntity;
+import io.toro.ojtbe.jimenez.Graphify.core.Handler;
 
-interface ServiceGenerator extends Generator {
+import java.util.List;
+
+interface ServiceGenerator extends Handler<List<GraphEntity>> {
     void generate(GraphEntity graphEntity)
             throws ServiceGeneratorException;
 
